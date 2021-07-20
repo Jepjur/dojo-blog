1145187b1dc0b1cf332b9715f9d61c818852db60
Jasper <Jasper_vandijk@hotmail.com>
2021-07-20 13:00:19 +0200

    added error handling for JSON fetch

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch(' http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then((data) => {
-            setBlogs(data);
-            setIsPending(false);
-        });
-        }, 1000)
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch data');
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+                .catch((err) => {
+                    setIsPending(false);
+                    setError(err.message);
+                    setError(null);
+                })
+            }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title= "All Blogs!" />}
         </div>
