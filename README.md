976c3b30b51b197ede7ffa3dcdbd8f65d2bae3d9
Jasper <Jasper_vandijk@hotmail.com>
2021-07-22 12:15:48 +0200

    added page routing

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# A very basic single page app where I fetch JSON data
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,17 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch(' http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch data');
-                    }
-                    return res.json()
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                    setError(null);
-                })
-            }, 1000)
-    }, []);
-
-    return ( 
-        <div className="home">
-            { error && <div>{error}</div>}
-            {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title= "All Blogs!" />}
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
